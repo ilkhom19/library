013be49a60c597a77981fdd909132fc2e720ffad
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2022-05-27 18:41:36 +0600

    added languages to books

--- catalog/models.py
+++ catalog/models.py
@@ -20,6 +20,7 @@ class Language(models.Model):
                             help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
 
     def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
         return self.name
 
 
@@ -35,6 +36,11 @@ class Book(models.Model):
                              help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
+    
+    class Meta:
+        ordering = ['title', 'author']
 
     def display_genre(self):
         """Create a string for the Genre. This is required to display genre in Admin."""
