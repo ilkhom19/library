f10c12d95141f485e36fb9f1c70422f40c80c435
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2022-05-31 12:13:55 +0600

    added renew page

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from datetime import date
 import uuid
 
 
@@ -34,7 +36,8 @@ class Book(models.Model):
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
-    
+
+
     class Meta:
         ordering = ['title', 'author']
 
@@ -65,6 +68,9 @@ class BookInstance(models.Model):
         ('r', 'Reserved'),
     )
 
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+
     status = models.CharField(
         max_length=1,
         choices=LOAN_STATUS,
@@ -73,11 +79,20 @@ class BookInstance(models.Model):
         help_text='Book availability',
     )
 
+
+
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         return f'{self.id} ({self.book.title})'
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 
 
@@ -96,8 +111,3 @@ class Author(models.Model):
 
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
-
-
-
-
-
